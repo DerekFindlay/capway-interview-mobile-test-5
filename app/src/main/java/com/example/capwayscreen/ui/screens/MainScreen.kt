12ed57019d0102b95a4c4ec8add0cef9f6aa7b5a
DerekFindlay <TellDerek2@gmail.com>
2022-04-14 00:41:10 -0400

    Clean up.

--- app/src/main/java/com/example/capwayscreen/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/capwayscreen/ui/screens/MainScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.rememberNavController
